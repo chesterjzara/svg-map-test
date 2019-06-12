3c7384548b4e1b6a15004dff5e1458e0a9e545ab
Chester Zara <chesterjzara@gmail.com>
2019-06-12 07:13:48 -0500

    add favicon and title, remove debugging logs

--- src/components/Country.js
+++ src/components/Country.js
@@ -7,7 +7,7 @@ class Country extends Component {
       <React.Fragment>
         <div className="modal">
             <h2>{this.props.currentCountry.country_title}</h2>
-            <img src={`https://www.countryflags.io/${this.props.currentCountry.country_code}/shiny/64.png`} />
+            <img src={`https://www.countryflags.io/${this.props.currentCountry.country_code}/shiny/64.png`} alt='country flag'/>
             <br />
           <button onClick={this.props.toggleModal}>Close Modal</button>
           {this.props.loggedInUser ?
